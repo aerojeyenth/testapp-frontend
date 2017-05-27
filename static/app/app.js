f07477b81c1ac67b4038f0c3dd9db052514bcf75
e066532 <Jeyenthaaran.Nanjappan@mastercard.com>
2017-05-27 23:40:55 +0530

    Modify search function to handle the SortBy and OrderBy functionality Implement SortBy and OrderBy functionality using RxJS combine latest. Set Default value for SortBy(Id) and OrderBy(asc) in View

--- static/app/app.js
+++ static/app/app.js
@@ -26,24 +26,54 @@ function appCtrl($http, $window) {
         $window.location.href = "/view/" + id;
     }
 
-    function search(query){
-        return $http.get('http://127.0.0.1:5000/api/v1/search/?query='+ query);
+    function search(queryObj){
+
+        var sort_by = queryObj.sortBy !== '' ? '&sort_by=' + queryObj.sortBy : '';
+        var order = queryObj.orderBy !== '' ? '&order=' + queryObj.orderBy : '';
+
+        return $http.get('http://127.0.0.1:5000/api/v1/search/?query='+ queryObj.query + sort_by + order);
     }
+
 
     //Getting the input element using its ID
     var input = document.getElementById("system-search");
+    var sortBy = document.getElementById("sel1");
+    var orderBy = document.getElementById("sel2");
 
     //Input event stream observable
-    var keyups$ = Rx.Observable.fromEvent(input, 'keyup')
+    var query$ = Rx.Observable.fromEvent(input, 'input')
+        .startWith({target:{value:''}})
         .map(function (e) {
             return e.target.value;
         });
 
-    //Converts the promise into first order observable
-    var searcher = keyups$.switchMap(search);
+    //Change event stream observable for sortBy
+    var sortBy$ = Rx.Observable.fromEvent(sortBy, 'change')
+                .startWith({target:{value:'id'}})
+                .map(function (e) {
+                    return e.target.value;
+                });
+    //Change event stream observable for orderBy
+    var orderBy$ = Rx.Observable.fromEvent(orderBy, 'change')
+                .startWith({target:{value:'asc'}})
+                .map(function (e) {
+                    return e.target.value;
+                });
 
-    //Subscribing to the response observable
-    searcher.subscribe(function(res) {
+    //Using Combine Latest to combine all the streams
+    var searcherCombined$ = Rx.Observable.combineLatest(query$, sortBy$, orderBy$, function (query, sortBy, orderBy) {
+        return {
+            query: query,
+            sortBy: sortBy,
+            orderBy: orderBy
+        };
+    });
+
+    //Converts the promise into first order observable
+    var searcher$ = searcherCombined$.switchMap(search);
+
+    // Subscribing to the response observable
+    searcher$.subscribe(function(res) {
         vm.searchResults = res.data.results;
     });
 
